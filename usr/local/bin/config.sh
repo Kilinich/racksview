7af9539de2640f802352c95b7afabc217433b428
IliaIlinich <ilya.ilinich@gmail.com>
2024-12-11 14:18:58 +0100

    Configuration file created; Width, height and frame rate added to configuration file and implemented into start .sh script;

---
+++ usr/local/bin/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+width=1024
+height=768
+framerate=1/1
--- usr/local/bin/start_gstreamer.sh
+++ usr/local/bin/start_gstreamer.sh
@@ -2,8 +2,10 @@
 
 export HOSTNAME=$(hostname)
 
+source ./config.sh
+
 gst-launch-1.0 libcamerasrc ! \
-video/x-raw,width=1024,height=768,framerate=1/1 ! \
+video/x-raw,width=$width,height=$height,framerate=$framerate ! \
 videoconvert ! \
 textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
 clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
