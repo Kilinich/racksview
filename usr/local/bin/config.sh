04ef29295e16586290f1b2d12b17c7df5264f1a3
IliaIlinich <ilya.ilinich@gmail.com>
2024-12-12 10:19:22 +0100

    More variable added to config file: jpengec quality host port

--- usr/local/bin/config.sh
+++ usr/local/bin/config.sh
@@ -3,3 +3,7 @@
 width=1024
 height=768
 framerate=1/1
+
+jpegenc_quality=50
+host=0.0.0.0
+port=8080
--- usr/local/bin/start_gstreamer.sh
+++ usr/local/bin/start_gstreamer.sh
@@ -9,6 +9,6 @@ video/x-raw,width=$width,height=$height,framerate=$framerate ! \
 videoconvert ! \
 textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
 clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
-videoconvert ! jpegenc quality=50 ! multipartmux ! queue ! \
-tcpserversink host=0.0.0.0 port=8080 \
+videoconvert ! jpegenc quality=$jpegenc_quality ! multipartmux ! queue ! \
+tcpserversink host=$host port=$port \
 2>/var/log/gstreamer-error.log
